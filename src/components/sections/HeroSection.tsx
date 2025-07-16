96abd47cded2ef79f67ccb3be1fa42046e5a6fb5
bala-guhan <gsbalaguhanesh@gmail.com>
2025-07-16 15:59:23 +0530

    v0

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -32,7 +32,7 @@ const HeroSection = () => {
           poster="/placeholder.svg"
           style={{ pointerEvents: 'none' }}
         >
-          <source src="../../public/videos/galaxy.mp4" type="video/mp4" />
+          <source src="videos/galaxy.mp4" type="video/mp4" />
         </video>
         {/* Fading Overlay Layer */}
         <div className="absolute inset-0 z-10 pointer-events-none" style={{
